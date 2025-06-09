6886a8d97dcb0ae3f6044eeb96d0f438330d11ab
ISHU <raoishu5600@gmail.com>
2025-06-09 14:37:35 +0530

    version update

--- server.js
+++ server.js
@@ -25,9 +25,6 @@ app.get('/', (req, res) => res.send('Book Review API Running'));
 // Start Server
 const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
   .catch(err => console.error('MongoDB connection error:', err));
